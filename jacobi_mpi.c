ff43f3b8aaeede2181aa95411590ec7308a18aac
gapdan <gapdan@gapdi-pc.gapdan>
2019-12-02 10:04:33 -0500

    MPI solution bug fixez, but still fails on Gatherv

--- jacobi_mpi.c
+++ jacobi_mpi.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <stdlib.h>
+#include <time.h>
 #define SIZE 1024
 
 int iterations = 50;
@@ -25,6 +26,7 @@ void generate_diagonal_dominant_matrix(const ulong size, float min, float max) {
 }
                             
 int main(int argc, char **argv) {
+    double stime, etime;
     int err, i, j;
     int sum = 0;
     int id, nrproc, n, nrecv;
@@ -33,18 +35,18 @@ int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
     err = MPI_Comm_size(MPI_COMM_WORLD, &nrproc);
     err = MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    n = SIZE;
+    sendcounts = (int *) malloc(nrproc * sizeof(int));
+    displs = (int *) malloc(nrproc * sizeof(int));
+    terms = (float*) malloc(n * sizeof(float));
     if (id == 0) {
         // Scatter data
         // Each process takes an equal number of columns
-        n = SIZE;
         coeff = (float*) malloc(n * n * sizeof(float));
-        terms = (float*) malloc(n * sizeof(float));
         for (j = 0; j < n; ++j) {
           terms[j] = -10000 + rand() % 20000;
         }
         generate_diagonal_dominant_matrix(n, -10000, 10000);
-        sendcounts = (int *) malloc(nrproc * sizeof(int));
-        displs = (int *) malloc(nrproc * sizeof(int));
         for (i = 0; i < nrproc; ++i) {
             sendcounts[i] = (n / nrproc) * n ;
             displs[i] = sum;
@@ -52,28 +54,33 @@ int main(int argc, char **argv) {
         }
         sendcounts[nrproc - 1] += (n % nrproc) * n;
     }
+    printf("Init done\n");
     // send specific data to processes
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&iterations, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&tolerance, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(terms, n, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-    float* recv = (float *) malloc(n * sizeof(float));
+    printf("Bcast done\n");
+    float* recv = (float *) calloc(sendcounts[id], sizeof(float));
     nrecv = (n / nrproc);
     if (id == nrproc - 1)
       nrecv += (n % nrproc);
 
+    printf("Scatterv\n");
     MPI_Scatterv(coeff, sendcounts, displs, MPI_FLOAT, 
                 recv, nrecv * n, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
+    printf("Scatterv done\n");
     float* solutions[2];
-    solutions[0] = (float*) calloc(n, sizeof(float));
+    solutions[0] = (float*) calloc(nrecv, sizeof(float));
     solutions[1] = (float*) calloc(nrecv, sizeof(float));
 
     float zero = tolerance - tolerance;
     float error;
 
     if (id == 0) {
+        sum = 0;
         for (i = 0; i < nrproc; ++i) {
             sendcounts[i] = (n / nrproc);
             displs[i] = sum;
@@ -81,7 +88,8 @@ int main(int argc, char **argv) {
         }
         sendcounts[nrproc - 1] += (n % nrproc);
     }
-
+    printf("Start algo\n");
+    stime = MPI_Wtime();
     //Starting iterations
     int iteration;
     for (iteration = 0; iteration < iterations; ++iteration) {
@@ -100,14 +108,18 @@ int main(int argc, char **argv) {
             }
             solutions[sol_it][i] =  (terms[i] + (solutions[osol_it][i] * recv[i * n + i])) / recv[i * n + j];
         }
-
-        MPI_Gatherv(solutions[sol_it] + start, (stop - start + 1), MPI_FLOAT,
-                  solutions[sol_it], sendcounts, displs, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        printf("Gather\n");
+        float *p = solutions[sol_it] + start;
+        MPI_Gatherv(p, (stop - start + 1), MPI_FLOAT, solutions[sol_it], 
+            sendcounts, displs, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        printf("Broadcast\n");
         MPI_Bcast(solutions[sol_it], n, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        printf("Barrier");
         MPI_Barrier(MPI_COMM_WORLD);
     }
-
+    etime = MPI_Wtime();
     //Gather solutions
-
+    printf("Total time is: %lf", etime - stime);
     MPI_Finalize();
+    return 0;
 }
